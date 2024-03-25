5e1fea26117b96edf765d8c51e48618bc201b9bc
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-25 20:31:26 +0800

    2 коммит через терминал Lesson 10

--- main.js
+++ main.js
@@ -12,4 +12,7 @@ const greeting1 = 'Hello';
 // Обратите внимание на ограничители строки, это бектики
 // Интерполяция не работает с одинарными и двойными кавычками
 console.log(`${greeting1}, ${firstName1}!`);
-// => Hello, Joffrey!
+// => Hello, Joffrey! 
+
+console.log(`- Are you hungry?
+- Aaaarrrgh!`);
